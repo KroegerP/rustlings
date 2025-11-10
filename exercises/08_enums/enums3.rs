b32544d2c3b1d8cae8e8c3f29396828e88cb3b44
Peter Kroeger <pkroeger@saec-kv.com>
2025-11-10 17:37:06 -0500

    structs and enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize {width, height} => self.resize(width, height),
+            Message::Move(i) => self.move_position(i),
+            Message::Echo(i) => self.echo(i),
+            Message::ChangeColor(r, g, b) => self.change_color(r,g, b),
+            Message::Quit => self.quit()
+        };
     }
 }
 
