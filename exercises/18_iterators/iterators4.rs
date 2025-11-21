7443afa3c6917625a427cef13b7f992bdd81c8af
Peter Kroeger <pkroeger@saec-kv.com>
2025-11-21 15:57:13 -0500

    iterators and box

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -10,6 +10,7 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+    (0..num).map(|i| i + 1).product()
 }
 
 fn main() {
